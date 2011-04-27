b8ac451e5fc01d7eaa6445bc50d03a0c1beddecb
Kevin Knoll <knollkevin@gmail.com>
2011-04-27 08:09:31 -0400

    added new images, customizable ratio, debugging links

--- index.php
+++ index.php
@@ -5,12 +5,21 @@
     <meta charset="utf-8">
     <link rel="stylesheet" href="style.css">
     <?php
-      $img = imagecreatefrompng('logo.png');
+      if (isset($_GET['src'])) {
+        $src = $_GET['src'];
+      } else {
+        $src = 'logo.png';
+      }
+      $img = imagecreatefrompng($src);
 
       $w = imagesx($img);
       $h = imagesy($img);
 
-      $ratio = 25;
+      if (isset($_GET['ratio'])) {
+        $ratio = $_GET['ratio'];
+      } else {
+        $ratio = 10;
+      }
 
       echo '<style>
         ul {
@@ -25,6 +34,17 @@
     ?>
   </head>
   <body>
+    <p>
+      <?php $qs_ratio = 'ratio=' . $ratio; ?>
+      <a href="?<?php echo $qs_ratio; ?>">Default</a>
+      <a href="?images&amp;<?php echo $qs_ratio; ?>">Default w/ Images</a>
+      <a href="?src=logo2.png&amp;<?php echo $qs_ratio; ?>">Logo2</a>
+      <a href="?src=logo2.png&amp;images&amp;<?php echo $qs_ratio; ?>">Logo2 w/ Images</a>
+      <a href="?src=logo3.png&amp;<?php echo $qs_ratio; ?>">Logo3</a>
+      <a href="?src=logo3.png&amp;images&amp;<?php echo $qs_ratio; ?>">Logo3 w/ Images</a>
+      <a href="?src=logo4.png&amp;<?php echo $qs_ratio; ?>">Logo4</a>
+      <a href="?src=logo4.png&amp;images&amp;<?php echo $qs_ratio; ?>">Logo4 w/ Images</a>
+    </p>
     <?php
       $IMAGES = isset($_GET['images']);
 
@@ -44,6 +64,14 @@
           'http://si3.twimg.com/profile_images/953696655/image_normal.jpg',
           'http://si0.twimg.com/profile_images/1246926444/fallon17_normal.jpg',
           'http://si1.twimg.com/profile_images/1324305552/iam_DAME_normal.jpg',
+          'http://a0.twimg.com/profile_images/1289788781/168024_1593477200204_1334138791_31476261_4453376_n_normal.jpg',
+          'http://a1.twimg.com/profile_images/1319556134/paramore_normal.jpg',
+          'http://a0.twimg.com/profile_images/1100679928/brandwan-sc_normal.jpg',
+          'http://a3.twimg.com/profile_images/83148253/muffandme_normal.jpg',
+          'http://a0.twimg.com/profile_images/1208984990/dailyboooth_normal.jpg',
+          'http://a2.twimg.com/profile_images/1251366721/Snapshot_20110216_3_normal.jpg',
+          'http://a1.twimg.com/profile_images/1308462157/13549_104108369605250_100000182616111_108371_13443_n_normal.jpg',
+
         );
         $tmpimgcnt = count($tmpimgs) - 1;
       }
@@ -61,20 +89,25 @@
           if ($a > 0) {
             $l = $ratio*$x;
             $t = $ratio*$y;
-            $s = 'style="left:' . $l . 'px;top:' . $t . 'px;opacity:' . $a . '"';
+            $s = 'left:' . $l . 'px;top:' . $t . 'px;opacity:' . $a . ';';
 
-            echo '<li ' . $s . '>';
+            echo '<li style="' . $s . '">';
+            
+            $s = 'background:rgb(' . $colors['red'] . ',' . $colors['green'] . ',' . $colors['blue'] . ');opacity:' . ($a/2) . ';';
+            echo '<span style="' . $s . '"></span>';
+
             if ($IMAGES) {
               echo '<img src="' . $tmpimgs[$i % $tmpimgcnt] . '" width="' . $ratio . '" height="' . $ratio . '">';
             }
+
             echo '</li>';
-            
             ++$i;
           }
         }
       }
 
       echo '</ul>';
+
     ?>
     <p><?php echo 'built with ' . $i . ' blocks'; ?></p>
   </body>
